9fcc0fdfbfbf7b6cc279857ae7ecf65a83a87e76
arnaud_h <arnaud_h@etna-alternance.net>
2018-03-13 23:02:48 +0100

    Update script_convert_ascii_to_json.sh

--- script_convert_ascii_to_json.sh
+++ script_convert_ascii_to_json.sh
@@ -1,7 +1,5 @@
-#python ./display_song.py /home/robinet/Million-Song-Dataset-HDF5-to-CSV/LES_FICHIER_DE_FDP/TRAAAAW128F429D538.h5
-
 n=0
-for file in `ls /home/robinet/MSongsDB/PythonSrc/FICHIER/`; do
+for file in `ls ./FICHIER/`; do
     var=$file".json"
     php convert_ascii_to_json.php /home/robinet/MSongsDB/PythonSrc/FICHIER/$file > ./FICHIERS_JSON/$var
     n=$((n+1))
