fa904c9e282cf0029c0cf48fb57c6b027cb3f8a2
arnaud_h <arnaud_h@etna-alternance.net>
2018-03-13 23:04:26 +0100

    Update script_convert_ascii_to_json.sh

--- script_convert_ascii_to_json.sh
+++ script_convert_ascii_to_json.sh
@@ -1,7 +1,7 @@
 n=0
 for file in `ls ./FICHIER/`; do
     var=$file".json"
-    php convert_ascii_to_json.php /home/robinet/MSongsDB/PythonSrc/FICHIER/$file > ./FICHIERS_JSON/$var
+    php convert_ascii_to_json.php ./FICHIER/$file > ./FICHIERS_JSON/$var
     n=$((n+1))
     echo $n
 done
