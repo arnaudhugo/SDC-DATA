28f4252a76cfdb80fbf81972b2d876f594ff24c2
arnaud_h <arnaud_h@etna-alternance.net>
2018-03-20 19:08:09 +0100

    Update

--- API/api.py
+++ API/api.py
@@ -5,11 +5,12 @@ from neo4jrestclient import client
 
 import json
 
-gdb = GraphDatabase("http://YOUR_IP:7474/")
+gdb = GraphDatabase("http://172.16.1.171:7474/")
 
 # Return all music with title not null
 @route('/api/')
 def index():
+    response.content_type = 'application/json'
     query = "MATCH (n) WHERE n.title IS NOT NULL RETURN n"
     results = gdb.query(query, data_contents=True)
     return (json.dumps(results.rows)).decode('unicode_escape')
@@ -17,6 +18,7 @@ def index():
 # Return all genre
 @route('/api/all/genre/')
 def all_genres():
+    response.content_type = 'application/json'
     query = "MATCH (n:Genre) RETURN n.name"
     results = gdb.query(query, data_contents=True)
     return "{ \"genres\": [" + (json.dumps(results.rows)).decode('unicode_escape').replace('[', '').replace(']', '') + "]}"
@@ -24,6 +26,7 @@ def all_genres():
 # Return 200 random music
 @route('/api/random/')
 def all_genres():
+    response.content_type = 'application/json'
     query = "MATCH (g:Genre)<-[:HAS_GENRE]-(a:Artist)-[:OWNS]->(m:Music) WITH g, a, m, rand() AS number RETURN { Genre : g.name, Artist : a.name, Titre : m.title } ORDER BY number LIMIT 200"
     results = gdb.query(query, data_contents=True)
     return "{ \"random\": [" + (json.dumps(results.rows)).decode('unicode_escape').replace('[', '').replace(']', '') + "]}"
@@ -31,6 +34,7 @@ def all_genres():
 # Return <nb> of music by <genre>
 @get('/api/genre/<genre>/<nb>')
 def genre(genre, nb):
+    response.content_type = 'application/json'
     genre = genre.replace('%20', ' ').replace('\'', '\\\'')
     query = "MATCH (n:Genre {name: '" + genre + "'})<-[:HAS_GENRE]-(artist)-[:OWNS]->(music) RETURN { Artist : artist.name, Titre : music.title } LIMIT " + nb + ""
     results = gdb.query(query, data_contents=True)
@@ -39,9 +43,10 @@ def genre(genre, nb):
 # Return <nb> of music by <artist>
 @get('/api/artist/<artist>/<nb>')
 def artist(artist, nb):
+    response.content_type = 'application/json'
     artist = artist.replace('%20', ' ').replace('\'', '\\\'')
     query = "MATCH (m:Music)<-[:OWNS]-(a:Artist {name: '" + artist + "'}) RETURN m.title LIMIT " + nb + ""
     results = gdb.query(query, data_contents=True)
     return "{ \"" + artist + "\": [" + (json.dumps(results.rows)).decode('unicode_escape').replace('[', '').replace(']', '') + "]}"
 
-run(host='YOUR_IP', port=5000)
+run(host='172.16.1.171', port=5000)
