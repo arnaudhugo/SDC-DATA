c5aa4ead067de113ca6c18ff4ba2b7fac0f2656e
arnaud_h <arnaud_h@etna-alternance.net>
2018-03-22 08:47:05 +0100

    Upadate

--- API/api.py
+++ API/api.py
@@ -11,6 +11,7 @@ gdb = GraphDatabase("http://172.16.1.171:7474/")
 @route('/api/')
 def index():
     response.content_type = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     query = "MATCH (n) WHERE n.title IS NOT NULL RETURN n"
     results = gdb.query(query, data_contents=True)
     return (json.dumps(results.rows)).decode('unicode_escape')
@@ -19,15 +20,17 @@ def index():
 @route('/api/all/genre/')
 def all_genres():
     response.content_type = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     query = "MATCH (n:Genre) RETURN n.name"
     results = gdb.query(query, data_contents=True)
     return "{ \"genres\": [" + (json.dumps(results.rows)).decode('unicode_escape').replace('[', '').replace(']', '') + "]}"
 
 # Return 200 random music
-@route('/api/random/')
-def all_genres():
+@route('/api/random/<nb>')
+def all_genres(nb):
     response.content_type = 'application/json'
-    query = "MATCH (g:Genre)<-[:HAS_GENRE]-(a:Artist)-[:OWNS]->(m:Music) WITH g, a, m, rand() AS number RETURN { Genre : g.name, Artist : a.name, Titre : m.title } ORDER BY number LIMIT 200"
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    query = "MATCH (g:Genre)<-[:HAS_GENRE]-(a:Artist)-[:OWNS]->(m:Music) WITH g, a, m, rand() AS number RETURN { Genre : g.name, Artist : a.name, Titre : m.title } ORDER BY number LIMIT " + nb + ""
     results = gdb.query(query, data_contents=True)
     return "{ \"random\": [" + (json.dumps(results.rows)).decode('unicode_escape').replace('[', '').replace(']', '') + "]}"
 
@@ -35,6 +38,7 @@ def all_genres():
 @get('/api/genre/<genre>/<nb>')
 def genre(genre, nb):
     response.content_type = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     genre = genre.replace('%20', ' ').replace('\'', '\\\'')
     query = "MATCH (n:Genre {name: '" + genre + "'})<-[:HAS_GENRE]-(artist)-[:OWNS]->(music) RETURN { Artist : artist.name, Titre : music.title } LIMIT " + nb + ""
     results = gdb.query(query, data_contents=True)
@@ -44,6 +48,7 @@ def genre(genre, nb):
 @get('/api/artist/<artist>/<nb>')
 def artist(artist, nb):
     response.content_type = 'application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     artist = artist.replace('%20', ' ').replace('\'', '\\\'')
     query = "MATCH (m:Music)<-[:OWNS]-(a:Artist {name: '" + artist + "'}) RETURN m.title LIMIT " + nb + ""
     results = gdb.query(query, data_contents=True)
